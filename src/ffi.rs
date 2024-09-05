70f76b210a263fd1d435577eac9ac1a828c6f444
syoch <syoch64@gmail.com>
2024-09-05 00:26:38 +0000

    Fix(ffi): Fixed double free

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,8 @@
-use core::{ffi::c_void, time::Duration};
+use core::{
+    ffi::c_void,
+    ptr::{null, null_mut},
+    time::Duration,
+};
 
 use alloc::{boxed::Box, ffi::CString};
 use srobo_base::{
@@ -23,7 +27,6 @@ pub extern "C" fn __ffi_cim920_new(
             &mut *time
         }),
     }));
-    // let im920 = null_mut() as *mut CIM920;
 
     im920
 }
@@ -34,40 +37,40 @@ pub extern "C" fn __ffi_cim920_on_data(
     cb: extern "C" fn(ctx: *const c_void, from: u16, data: *const u8, len: usize) -> (),
     ctx: *const c_void,
 ) -> () {
-    let mut im920 = unsafe { Box::from_raw(instance) }.im920;
-    im920.on_data(Box::new(move |data| {
-        cb(
-            ctx,
-            data.packet.node_id,
-            data.packet.data.as_ptr(),
-            data.packet.data.len(),
-        );
-    }));
+    unsafe { &mut *instance }
+        .im920
+        .on_data(Box::new(move |data| {
+            cb(
+                ctx,
+                data.packet.node_id,
+                data.packet.data.as_ptr(),
+                data.packet.data.len(),
+            );
+        }));
 }
 
 #[no_mangle]
 pub extern "C" fn __ffi_cim920_get_node_number(instance: *mut CIM920, duration_secs: f32) -> u16 {
-    let mut im920 = unsafe { Box::from_raw(instance) }.im920;
-    let node_number = im920.get_node_number(Duration::from_secs_f32(duration_secs));
+    let cim920 = unsafe { &mut *instance };
+    let node_number = cim920
+        .im920
+        .get_node_number(Duration::from_secs_f32(duration_secs));
 
-    match node_number {
-        Ok(node_number) => node_number,
-        Err(_) => 0,
-    }
+    node_number.unwrap_or(0)
 }
 
 #[no_mangle]
 pub extern "C" fn __ffi_cim920_get_version(instance: *mut CIM920, duration_secs: f32) -> *mut i8 {
-    let mut im920 = unsafe { Box::from_raw(instance) }.im920;
-
-    let string = match im920.get_version(Duration::from_secs_f32(duration_secs)) {
+    let cim920 = unsafe { &mut *instance };
+    let string = match cim920
+        .im920
+        .get_version(Duration::from_secs_f32(duration_secs))
+    {
         Ok(version) => version,
         Err(_) => return core::ptr::null_mut(),
     };
 
-    let string = CString::new(string).unwrap();
-
-    string.into_raw()
+    return string.as_ptr() as *mut i8;
 }
 #[no_mangle]
 pub extern "C" fn __ffi_cim920_transmit_delegate(
@@ -77,15 +80,13 @@ pub extern "C" fn __ffi_cim920_transmit_delegate(
     len: usize,
     duration_secs: f32,
 ) -> bool {
-    let mut im920 = unsafe { Box::from_raw(instance) }.im920;
-
     let packet = Packet {
         node_id: dest,
-        data: unsafe { core::slice::from_raw_parts(data, len) }.to_vec(),
+        data: unsafe { core::slice::from_raw_parts(data, len) },
     };
 
-    match im920.transmit_delegate(packet, Duration::from_secs_f32(duration_secs)) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+    unsafe { &mut *instance }
+        .im920
+        .transmit_delegate(packet, Duration::from_secs_f32(duration_secs))
+        .is_ok()
 }
