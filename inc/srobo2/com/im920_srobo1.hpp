d5774d0c6a521e8a7160f35711e1e7228ff7c60e
syoch <syoch64@gmail.com>
2024-09-10 02:34:55 +0000

    Update: Added syoch-robotics (srobo1) port

---
+++ inc/srobo2/com/im920_srobo1.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <robotics/network/stream.hpp>
+
+#include <srobo2/com/im920.hpp>
+
+namespace srobo2::com {
+class IM910_SRobo1 : public robotics::network::Stream<uint8_t, uint16_t> {
+  srobo2::com::CIM920 *im920_;
+
+ public:
+  IM910_SRobo1(srobo2::com::CIM920 *im920) : im920_(im920) {
+    im920->OnData([this](uint16_t from, uint8_t *data, size_t len) {
+      logger.Info("Received data from %d", from);
+      logger.Hex(robotics::logger::core::Level::kInfo, data, len);
+      this->DispatchOnReceive(from, data, len);
+    });
+  }
+
+  uint16_t GetNodeNumber() { return im920_->GetNodeNumber(1.0f); }
+
+  std::string GetVersion() { return im920_->GetVersion(1.0f); }
+
+  void Send(uint16_t dest, uint8_t *data, uint32_t len) override {
+    im920_->Send(dest, data, len, 1.0f);
+  }
+};
+}  // namespace srobo2::com
